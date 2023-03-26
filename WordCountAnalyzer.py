8b05a0c3099873c3f68a88eceb21826bbda22827
Cherifa Bochra Soltani <65483776+soltani-bochra@users.noreply.github.com>
2023-03-26 12:45:18 +0300

    Add files via upload

---
+++ WordCountAnalyzer.py
@@ -0,0 +1,11 @@
+# Read the input string from the user
+data = input('Enter a string: ')
+
+# Split the string into individual words
+words = data.split()
+
+# Count the number of words
+count = len(words)
+
+# Print the result
+print('Number of words:', count)
