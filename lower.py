96877cf66b754c743db7c64e40cd532f2ae75bd7
Cherifa Bochra Soltani <65483776+soltani-bochra@users.noreply.github.com>
2023-03-26 11:53:41 +0300

    Add files via upload

---
+++ lower.py
@@ -0,0 +1,8 @@
+# Prompt user for input
+input_str = input("Enter an uppercase string: ")
+
+# Convert to lowercase
+lowercase_str = input_str.lower()
+
+# Print result
+print("Lowercase string:", lowercase_str)
