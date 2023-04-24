80543300c69f2198305a1e89352da317c34f0409
Cherifa Bochra Soltani <65483776+soltani-bochra@users.noreply.github.com>
2023-04-24 15:19:56 +0300

    Delete AutoEnableEventViewer.ps1

--- AutoEnableEventViewer.ps1
+++
@@ -1,9 +0,0 @@
-#This script sets a registry key to disable User Account Control (UAC) prompt for Event Viewer and then opens Event Viewer with logging enabled. Note that this script requires administrative privileges to run successfully.
-
-$registryPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System"
-$name = "EnableLUA"
-$value = 0
-Set-ItemProperty -Path $registryPath -Name $name -Value $value
-
-$cmd = "eventvwr.msc /c /enablelogging"
-Start-Process -FilePath $cmd -Verb RunAs
